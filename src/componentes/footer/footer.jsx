a828f6b0aac02d918c0134a313ab0029aeaceb88
Fe-Corazza <felipecorazza09@gmail.com>
2023-09-01 16:47:50 -0300

    App finalizado

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -1,12 +1,47 @@
 import { Children } from "react";
-import "../footer/footer.module.css";
+import styles from "../footer/footer.module.css";
 import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js";
 import SocialLinks from "../socialLinks/socialLinks";
 
 const Footer = ({ children, link }) => {
   return (
-    <div>
-      <footer>
+    <footer>
+      <ul>
+        <li>
+          <a href="">Conta</a>
+        </li>
+        <li>
+          <a href="">Carreiras</a>
+        </li>
+        <li>
+          <a href="">Informações corporativas</a>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <a href="">Termos de uso</a>
+        </li>
+        <li>
+          <a href="">Influência</a>
+        </li>
+        <li>
+          <a href="">Audio</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="">Central de ajuda</a>
+        </li>
+        <li>
+          <a href="">Privacidade</a>
+        </li>
+        <li>
+          <a href="">Avisos legais</a>
+        </li>
+      </ul>
+
+      <div className={styles.pipigrandao}>
         <p>
           Feito com <ion-icon name="heart" /> por{" "}
           <a href={link} target="_blank">
@@ -30,50 +65,8 @@ const Footer = ({ children, link }) => {
           href={"https://www.tiktok.com/@fe_corazza"}
         />
         <SocialLinks icon={"logo-linkedin"} href={"#"} />
-  </footer>
-  <footer>
-        <div className="boxs">
-          <ul>
-            <li>
-              <a href="#">Conta</a>
-            </li>
-            <li>
-              <a href="#">Carreiras</a>
-            </li>
-            <li>
-              <a href="#">Informação Corporavita</a>
-            </li>
-          </ul>
-        </div>
-        <div className="boxs">
-          <ul>
-            <li>
-              <a href="#">Termos de Uso</a>
-            </li>
-            <li>
-              <a href="#">Influência</a>
-            </li>
-            <li>
-              <a href="#">Audio</a>
-            </li>
-          </ul>
-        </div>
-        <div className="boxs">
-          <ul>
-            <li>
-              <a href="#">Central de ajuda</a>
-            </li>
-            <li>
-              <a href="#">Privacidade</a>
-            </li>
-            <li>
-              <a href="#">Aviso Legais</a>
-            </li>
-          </ul>
-        </div>
-        <div className="boxs"></div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 };
 
